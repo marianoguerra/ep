c3b6b08a32d5a2c4fb62e6dbf40ee1d5e29c128c
Mariano Guerra <mariano@marianoguerra.org>
2018-04-10 20:00:28 +0200

    copy clause match args and guards in the generated module, generate vars to match whole arg if not one there already

--- src/ep_compiler.erl
+++ src/ep_compiler.erl
@@ -1,15 +1,15 @@
 -module(ep_compiler).
--export([compile/2, compile_asts/2, ast_to_erl/1]).
+-export([compile/3, compile_asts/3, ast_to_erl/1]).
 -export([gen_proto_fun_name/2, gen_var_args/2]).
 -export([compile_forms/1, load_module/2, soft_purge_module/1, purge_module/1]).
 
-compile(BasePath, ProtoName) ->
+compile(BasePath, ProtoName, Opts) ->
     PathBlob = filename:join([BasePath, ProtoName, "*.ep"]),
     ProtoPaths = filelib:wildcard(PathBlob),
     FileContents = [file:consult(Path) || Path <- ProtoPaths],
     % handle errors
     Asts = [Ast || {ok, [Ast]} <- FileContents],
-    AstMap = compile_asts(Asts, ProtoName),
+    AstMap = compile_asts(Asts, ProtoName, Opts),
     ast_map_to_mod_ast(AstMap, ProtoName).
 
 compile_forms(Forms) ->
@@ -33,12 +33,12 @@ ast_map_to_mod_ast(AstMap, ProtoName) ->
      {attribute, 2, export, maps:keys(AstMap)}|
      maps:values(AstMap)].
 
-compile_asts(Asts, ProtoName) ->
-    compile_asts(Asts, #{}, ProtoName).
+compile_asts(Asts, ProtoName, Opts) ->
+    compile_asts(Asts, #{}, ProtoName, Opts).
 
-compile_asts([], Accum, ProtoName) ->
-    add_generic_router_clause_to_funs(Accum, ProtoName);
-compile_asts([#{funs := Funs, module := Module} | T], Accum, ProtoName) ->
+compile_asts([], Accum, ProtoName, Opts) ->
+    add_generic_router_clause_to_funs(Accum, ProtoName, Opts);
+compile_asts([#{funs := Funs, module := Module} | T], Accum, ProtoName, Opts) ->
     FoldFn = fun ({PFunName, {function, _Line, _ImplFunName, Arity, [Clause]}},
                   AccumIn) ->
                      % we use the arity of the impl since it should be
@@ -46,26 +46,33 @@ compile_asts([#{funs := Funs, module := Module} | T], Accum, ProtoName) ->
                      add_fun_clause(PFunName, Module, ProtoName, Arity, Clause, AccumIn)
              end,
     NewAccum = lists:foldl(FoldFn, Accum, maps:to_list(Funs)),
-    compile_asts(T, NewAccum, ProtoName).
+    compile_asts(T, NewAccum, ProtoName, Opts).
 
-add_generic_router_clause_to_funs(Accum, ProtoName) ->
+add_generic_router_clause_to_funs(Accum, ProtoName, Opts) ->
+    TVType= maps:get(tv_type, Opts, map),
     maps:map(fun ({PFunName, Arity}, Ast) ->
                      {function, Line, PFunName, Arity, Clauses} = Ast,
-					 NewClause = generic_router_clause(Line, Arity, ProtoName, PFunName),
+					 NewClause = generic_router_clause(Line, Arity, ProtoName, PFunName, TVType),
                      NewClauses = Clauses ++ [NewClause],
                      {function, Line, PFunName, Arity, NewClauses}
 
              end, Accum).
 
-add_fun_clause(PFunName, Module, ProtoName, Arity, _OrigClause, Accum) ->
+add_fun_clause(PFunName, Module, ProtoName, Arity,
+               {clause, _CL, CHeadArgs, CGuards, _CBody}, Accum) ->
     Line = 3,
     FunKey = {PFunName, Arity},
-    Args = gen_var_args(Line, Arity),
+    % we need to put the clause head arguments in the generated clause but
+    % we need to pass it to the implementation, so we need to match with
+    % a variable that won't clash each arg that is not already matching,
+    % we will use underscore
+    {HeadArgs, VarArgs} = match_clause_head_args(CHeadArgs, {[], [], 0}),
     FunName = gen_proto_fun_name(ProtoName, PFunName),
     % we route to the generated proto impl name since we know it will be
     % exported and for uniformity
-    Clause = ast_match_args_call_remote(Line, Args, {atom, Line, Module},
-                                        {atom, Line, FunName}, Args),
+    Clause = ast_match_args_call_remote(Line, HeadArgs, CGuards,
+                                        {atom, Line, Module},
+                                        {atom, Line, FunName}, VarArgs),
     NewAst = case maps:get(FunKey, Accum, not_found) of
                  not_found ->
                      {function, Line, PFunName, Arity, [Clause]};
@@ -74,34 +81,55 @@ add_fun_clause(PFunName, Module, ProtoName, Arity, _OrigClause, Accum) ->
              end,
     Accum#{FunKey => NewAst}.
 
+match_clause_head_args([], {HeadArgs, VarArgs, _Idx}) ->
+    {lists:reverse(HeadArgs), lists:reverse(VarArgs)};
+match_clause_head_args([Ast={match, _Line, VarAst={var, _Line1, _VName}, _RightAst}|T],
+                       {HeadArgs, VarArgs, Idx}) ->
+    NewAccum = {[Ast|HeadArgs], [VarAst|VarArgs], Idx + 1},
+    match_clause_head_args(T, NewAccum);
+match_clause_head_args([Ast|T], {HeadArgs, VarArgs, Idx}) ->
+    Line = element(2, Ast),
+    VarAst  = gen_var(Line, Idx),
+    NewAccum = {[{match, Line, VarAst, Ast}|HeadArgs], [VarAst|VarArgs], Idx + 1},
+    match_clause_head_args(T, NewAccum).
+
 gen_proto_fun_name(ProtoName, PFunName) when is_atom(ProtoName) ->
     gen_proto_fun_name(atom_to_list(ProtoName), PFunName);
 gen_proto_fun_name(ProtoNameStr, PFunName) ->
 	PFunNameStr = atom_to_list(PFunName),
 	list_to_atom(ProtoNameStr ++ "@" ++ PFunNameStr).
 
-generic_router_clause(Line, Arity, ProtoName, PFunName) ->
+generic_router_clause(Line, Arity, ProtoName, PFunName, TVType) ->
     FunName = gen_proto_fun_name(ProtoName, PFunName),
-
-	FirstArg = {match,Line,
-				{var,Line,'V'},
-				{map,Line,
-				 [{map_field_exact,Line,
-				   {atom,Line,'__struct__'},
-				   {var,Line,'Type'}}]}},
-
+	FirstArg = gen_tv_match(Line, TVType),
 	RestArgs = gen_var_args(Line, Arity - 1),
 
 	HeadArgs = [FirstArg | RestArgs],
 	CallArgs = [{var,Line,'V'}|RestArgs],
     ModAst = {var,Line,'Type'},
     FunAst = {atom,Line,FunName},
-    ast_match_args_call_remote(Line, HeadArgs, ModAst, FunAst, CallArgs).
+    ast_match_args_call_remote(Line, HeadArgs, [], ModAst, FunAst, CallArgs).
 
-ast_match_args_call_remote(Line, HeadArgs, ModAst, FunAst, CallArgs) ->
-	{clause, Line, HeadArgs, [],
+ast_match_args_call_remote(Line, HeadArgs, Guards, ModAst, FunAst, CallArgs) ->
+	{clause, Line, HeadArgs, Guards,
 	 [{call,Line, {remote,Line,ModAst,FunAst}, CallArgs}]}.
 
+gen_var(Line, I) ->
+    {var, Line, list_to_atom("Arg@" ++ integer_to_list(I))}.
+
 gen_var_args(Line, Count) ->
-	[{var, Line, list_to_atom("Arg" ++ integer_to_list(I))} ||
-	 I <- lists:seq(1, Count)].
+	[gen_var(Line, I) || I <- lists:seq(1, Count)].
+
+gen_tv_match(Line, map) ->
+    {match,Line,
+     {var,Line,'V'},
+     {map,Line,
+      [{map_field_exact,Line,
+        {atom,Line,'__struct__'},
+        {var,Line,'Type'}}]}};
+gen_tv_match(Line, tuple) ->
+    {match,Line,
+     {var,Line,'V'},
+     {tuple,Line,
+      [{tuple,Line,[{var,Line,'Type'},{var,Line,'_Data'},{var,Line,'_'}]}]}}.
+
--- test/ep_compiler_SUITE.erl
+++ test/ep_compiler_SUITE.erl
@@ -1,37 +1,37 @@
 -module(ep_compiler_SUITE).
 -compile(export_all).
 
-all() -> [ep_test_1].
+all() -> [ep_test_1_map, ep_test_1_tuple].
 
 data_dir(Config) -> proplists:get_value(data_dir, Config).
 priv_dir(Config) -> test_server:lookup_config(priv_dir, Config).
 
-ep_test_1(Config) ->
+ep_test_1_map(Config) ->
+    ep_test_1(Config, map, #{'__struct__' => my_mod}).
+
+ep_test_1_tuple(Config) ->
+    ep_test_1(Config, tuple, {{my_mod, my_data, my_meta}}).
+
+ep_test_1(Config, TVType, Struct) ->
     DataDir = data_dir(Config),
-    ModAst = ep_compiler:compile(DataDir, "consy"),
+    ModAst = ep_compiler:compile(DataDir, "consy", #{tv_type => TVType}),
     %ok = ModAst,
     ErlCode = ep_compiler:ast_to_erl(ModAst),
     ct:pal("~s~n", [ErlCode]),
     {ok, ModName, ModBin, _Warnings} = ep_compiler:compile_forms(ModAst), 
     ep_compiler:purge_module(ModName),
     {ok, Mod} = ep_compiler:load_module(ModName, ModBin),
+    call_undef(Mod, first, [[1, 2]], ep_test, consy@first, 1),
+    call_undef(Mod, rest, [[1, 2, 3]], ep_test, consy@rest, [2, 3]),
+    call_undef(Mod, first, [Struct], my_moda, consy@first, [Struct]).
+
+call_undef(Mod, Fun, Args, ModCall, FunCall, Expected) ->
     try 
         % it should call the implementation, but the module is not loaded..
-        1 = Mod:first([1, 2])
+        Expected = apply(Mod, Fun, Args)
     catch
         error:undef ->
             % .. so we check it fails trying to call the right implementation
             StackTrace = erlang:get_stacktrace(),
-            [{ep_test, consy@first, [[1, 2]], []} | _] = StackTrace
-    end,
-
-    try 
-        % it should call the implementation, but the module is not loaded..
-        [2, 3] = Mod:rest([1, 2, 3])
-    catch
-        error:undef ->
-            StackTrace1 = erlang:get_stacktrace(),
-            % .. so we check it fails trying to call the right implementation
-            [{ep_test, consy@rest, [[1, 2, 3]], []} | _] = StackTrace1
+            [{ModCall, FunCall, Args, []} | _] = StackTrace
     end.
-
